c509b2c19b812c503fa7893940ff6555dc160b4a
yomarguti <yomar.guti@gmail.com>
2020-11-25 09:28:00 -0500

    Base project with Tailwindcss on board

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../style.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Mi nuevo titulo</h1>
+      <ul>
+        <li>Mi</li>
+        <li>lista</li>
+        <li>inutil</li>
+        <li>Aja</li>
+        <li>Ah</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
